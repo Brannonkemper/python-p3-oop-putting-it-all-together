c633aaf8ea7d135ae954dee897a7055799791993
Brannonkemper <brannonsimiyu88@gmail.com>
2025-02-04 22:33:38 +0300

    puttingalltogether

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
+import sys
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # This will call the setter method below
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")  # This ensures output is captured in tests
+        else:
+            self._size = value  # Only assign if valid
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")  # Ensure expected test output
